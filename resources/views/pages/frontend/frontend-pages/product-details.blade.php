894df1d93695b9cb165781b5d79146b2132a4772
samikos <samik@onesolutionindia.in>
2022-03-23 09:37:18 +0530

    product details update

--- resources/views/pages/frontend/frontend-pages/product-details.blade.php
+++ resources/views/pages/frontend/frontend-pages/product-details.blade.php
@@ -3,6 +3,11 @@
 
 @section('content')
   <div id="product_single_page">
-    @include( 'frontend-templates.single-product.' .$appearance_settings['single_product']. '.' .$appearance_settings['single_product'] )
+    {{-- @include( 'frontend-templates.single-product.' .$appearance_settings['single_product']. '.' .$appearance_settings['single_product'] ) --}}
+    @include( 'frontend-templates.single-product.burst-beetee.burst-beetee' )
   </div>
+@endsection
+
+@section('scripts')
+<script src="{{ URL::asset('public/burst-beetee/js/jquery.bootstrap-touchspin.js') }}"></script>
 @endsection
